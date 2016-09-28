570a3265ccd57adcf812ddda78f1fc0fe200e8fd
Formicidae <eddie9ner@gmail.com>
2016-09-28 10:49:10 -0500

    mod broken

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <math.h>
 
 using namespace std;
 
@@ -24,32 +25,44 @@ int main()
             //getline(puzzles,line);
             //cout << line;
             int* ptr;
-            ptr = &sudoku;
-            iline << puzzles;
+            ptr = sudoku;
+            puzzles >> iline;
             for(int j = 0; j < 9;j++)
             {
                 //puzzles >> iline;
                 //sudoku[i][j] = iline /
-                cout << iline << endl;
+                cout << "line: " << iline << endl;
                 //chars = line.substr(j).c_str();
                 //ch = chars[0];
                 //cout << ch << endl;
                 //sudoku[i][j] = ch;
-                *(ptr + (i*9) + j) = iline / (100000000 / (10*j));
-                iline %= (100000000 / (10*j));
+                cout << iline / (100000000 / (int)pow(10,j)) << endl;
+                *(ptr + (i*9) + (j)) = iline / (100000000 / (int)pow(10,j));
+                //cout << *(ptr) << endl;
+                iline %= (100000000 / (int)pow(10,j));
+                cout << sudoku[j] << endl;
             }
 
         }
-        cout << sudoku[0][0] << endl;
-        cout << sudoku[0][1] << endl;
-        cout << sudoku[0][2] << endl;
-        cout << sudoku[0][3] << endl;
-        cout << sudoku[0][4] << endl;
-        cout << sudoku[0][5] << endl;
-        cout << sudoku[0][6] << endl;
-        cout << sudoku[0][7] << endl;
-        cout << sudoku[0][8] << endl;
-        cout << sudoku[0][9] << endl;
+        cout << "Line: " << endl;
+        cout << sudoku[0] << endl;
+        cout << sudoku[1] << endl;
+        cout << sudoku[2] << endl;
+        cout << sudoku[3] << endl;
+        cout << sudoku[4] << endl;
+        cout << sudoku[5] << endl;
+        cout << sudoku[6] << endl;
+        cout << sudoku[7] << endl;
+        cout << sudoku[8] << endl;
+
+        for(int i =0;i<9;i++)
+        {
+            for(int j =0;j<9;j++)
+            {
+                cout << sudoku[(i*9) + j];
+            }
+            cout << endl;
+        }
         looping = false;
 
     }
